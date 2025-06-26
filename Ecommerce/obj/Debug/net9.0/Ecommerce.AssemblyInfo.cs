61f2f8707b41bd2011bae00766116d6dba2320fc
brunowvisk <brunobfr@gmail.com>
2025-06-25 22:47:02 -0400

    Change project directory

--- Ecommerce/obj/Debug/net9.0/Ecommerce.AssemblyInfo.cs
+++ Ecommerce/obj/Debug/net9.0/Ecommerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a90287467da739f8bea92986e74cb2b46d46ea8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+403c7a2afe86c8e535021db9745fd175c49e02e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
